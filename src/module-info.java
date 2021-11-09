a78b10c4a19aa47d0198f88c87a276d07780f042
Helena Nakamura <helena.reiko@gmail.com>
2021-11-09 10:21:00 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,17 @@
 module wshopJavafxJdbc {
+	
+    
+    exports application;
+
+	requires javafx.base;
+	requires javafx.graphics;
+	
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+ 
+	//no meu caso eu não tinha adicionado essa linha
+        opens gui to javafx.graphics, javafx.fxml;
+
 }
